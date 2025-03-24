9ccc08a14adbfdc0ffd3b2af6750b2c3d812438f
Q4181 <kewliekung@gmail.com>
2025-03-24 09:34:36 +0700

    first commit

---
+++ app/attractions/page.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/attractions')
+  return res.json()
+}
+
+export default async function Page() {
+  const data = await getData()
+  console.log(data)
+  return (
+    <div>
+      <h1>Attractions</h1>
+      <ul>
+        {data.map(attraction => (
+          <li key={attraction.id}>
+            <img src={attraction.coverimage} alt={attraction.name} width={100}/> 
+            <p>{attraction.name}
+            <h4><Link href={`/attractions/${attraction.id}`}> Read More</Link></h4></p>
+            
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
