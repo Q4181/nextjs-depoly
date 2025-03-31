d451a8122f3095d908efcaeb3937a57c3ce4e7fa
Q4181 <kewliekung@gmail.com>
2025-03-31 10:07:40 +0700

    Q

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -1,10 +1,34 @@
 import { NextResponse } from "next/server";
 import { mysqlPool } from "@/utils/db";
 
-export async function GET(request) {
-  const promisePool = mysqlPool.promise()
-  const [rows, fields] = await promisePool.query(
-    `SELECT * FROM attractions;`
-  )
-  return NextResponse.json(rows)
+const db = mysqlPool.promise()
+
+export async function GET(request){
+    try {
+        const [rows, fields] = await db.query(
+            'SELECT * FROM attractions'
+        )
+        return NextResponse.json(rows, {status: 200})
+    } catch (error){
+        return NextResponse.json({error:"Failed to fetch"}, {status: 500})
+    }
 }
+
+export async function PUT(request){
+  try {
+      const {id, name, detail, coverimage, latitude, longitude} = await request.json();
+      const [result] = await db.query(
+          'UPDATE attractions SET name=?, detail=?, coverimage=?, latitude=?, longitude=? WHERE id = ?',[name, detail, coverimage, latitude, longitude, id]
+      );
+      if (result.affectedRows == 0){
+
+        return NextResponse.json({error: "ID Not found"}, {status: 404});
+      }
+
+      return NextResponse.json({message: "Updated", id}, {status: 200});
+  } catch (error){
+      return NextResponse.json({error: error}, {status: 500})
+  }
+}
+
+
